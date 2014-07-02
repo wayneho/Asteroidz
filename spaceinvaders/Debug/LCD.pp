34d2344de5cc9b2058d17a4cdf491b7ba262fb15
Wayne Ho <wayneho92@hotmail.com>
2014-07-02 16:34:00 -0700

    Remove wrong directory

--- spaceinvaders/Debug/LCD.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-LCD.obj: ../LCD.c
-LCD.obj: ../LCD.H
-LCD.obj: ../tm4c123gh6pm.h
-
-../LCD.c: 
-../LCD.H: 
-../tm4c123gh6pm.h: 
--- spaceinvaders/Debug/interrupts.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-interrupts.obj: ../interrupts.c
-interrupts.obj: ../tm4c123gh6pm.h
-
-../interrupts.c: 
-../tm4c123gh6pm.h: 
